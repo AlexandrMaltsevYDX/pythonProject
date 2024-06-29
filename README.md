8221d2e77649bff66ab75a59a05704642f5a5a9b
AlexandrMaltsevYDX <AlexandrMaltsve@yandex.ru>
2024-06-29 19:00:02 +0200

    add alembic

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 poetry add "sqlalchemy[asyncio]"
 poetry add fastapi "uvicorn[standard]" "pydantic[email]" pydantic-settings
 poetry add asyncpg
+
+marked project-app as Source Root
---
+++ project-app/apps/app_users/models/users.py
@@ -0,0 +1,13 @@
+from sqlalchemy import UniqueConstraint
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+from core.db.model import Base
+
+
+class User(Base):
+    username: Mapped[str] = mapped_column(unique=True)
+    foo: Mapped[int]
+    bar: Mapped[int]
+
+    __table_args__ = (UniqueConstraint('foo', 'bar'),)
