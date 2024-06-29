706734bd7b1b4cbbdd7b9db6aa04baf76f525f0a
AlexandrMaltsevYDX <AlexandrMaltsve@yandex.ru>
2024-06-29 17:35:43 +0200

    add base model

--- project-app/core/settings/app_config.py
+++ project-app/core/settings/app_config.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
-from pydantic_settings import (
-    BaseSettings,
-)
+from pydantic import PostgresDsn
+from pydantic_settings import BaseSettings
+from pydantic_settings import SettingsConfigDict
 
 
 class RunConfig(BaseModel):
@@ -13,9 +13,32 @@ class ApiPrefix(BaseModel):
     prefix: str = '/api'
 
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+    naming_convention: dict[str, str] = {
+        'ix': 'ix_%(column_0_label)s',
+        'uq': 'uq_%(table_name)s_%(column_0_N_name)s',
+        'ck': 'ck_%(table_name)s_%(constraint_name)s',
+        'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+        'pk': 'pk_%(table_name)s',
+    }
+
+
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=('.env.this-project',),  # u can use inheritance envs files
+        case_sensitive=False,
+        env_nested_delimiter='__',
+        env_prefix='APP_CONFIG__',
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
 
 
 settings = Settings()
--- project-app/main.py
+++ project-app/main.py
@@ -1,15 +1,28 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
+from core.db.helper import db_helper
 from core.router import main_router
 from core.settings import settings
 
-app = FastAPI()
-app.include_router(main_router, prefix=settings.api.prefix)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):  # noqa: ARG001, ANN201
+    # startup
+    yield
+    # shutdown
+    print('dispose engine')  # noqa: T201
+    await db_helper.dispose()
+
+
+main_app = FastAPI(lifespan=lifespan)
+main_app.include_router(main_router, prefix=settings.api.prefix)
 
 if __name__ == '__main__':
     uvicorn.run(
-        'main:app',
+        'main:main_app',
         host=settings.run.host,
         port=settings.run.port,
         reload=True,
